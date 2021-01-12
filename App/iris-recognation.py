3038927523abff8da0f584179d1ee89d761cd54d
Abdo Mosa <abdomosa598@gmail.com>
2021-01-12 12:09:48 +0200

    merge structures

---
+++ App/iris-recognation.py
@@ -0,0 +1,7 @@
+import G6_iris_recognition
+
+def trainModel():
+    G6_iris_recognition.iris_model_train('Input_database/', 'encodingModel/irisEncodings.pickle')
+
+def testIris(path):
+    iris_name = G6_iris_recognition.iris_model_test('encodingModel/irisEncodings.pickle', 'data_temp/1r.jpg')
